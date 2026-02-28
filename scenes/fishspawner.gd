19a826c1b69b83d2d948e148bc587426183be422
Cлава Фёдоров <finfedslava100@gmail.com>
2026-02-28 21:24:56 +0200

    Fix the spawning of fishes, now they spown outside

--- scenes/fishspawner.gd
+++ scenes/fishspawner.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-# Загружаем сцену рыбы
+@export var player: Node2D 
+@export var spawn_offset_x: float = 600
 @export var fish_scene: PackedScene
-
-# Пределы спавна по Y
 @export var min_y: float = 350
 @export var max_y: float = 700
 
-# Интервал спавна
 @export var spawn_interval: float = 2.0
 var spawn_timer = 0.0
 
@@ -19,16 +17,30 @@ func _process(delta):
 
 func spawn_fish():
 	var fish = fish_scene.instantiate()
-	# Определяем направление случайно
-	if randi() % 2 == 0:
-		# Спавн слева -> плывёт вправо
-		fish.position = Vector2(-50, randf_range(min_y, max_y))
-		fish.direction = Vector2.RIGHT
-		fish.scale.x = 1  # текстура не отражена
-	else:
-		# Спавн справа -> плывёт влево
-		fish.position = Vector2(get_viewport().size.x + 50, randf_range(min_y, max_y))
-		fish.direction = Vector2.LEFT
-		fish.scale.x = -1  # отражаем текстуру по X
+	var y = randf_range(min_y, max_y)
+	var spawn_x: float
 
+	# Decide which side to spawn on
+	if randi() % 2 == 0:
+		# Spawn on the left
+		spawn_x = player.position.x - spawn_offset_x
+		fish.direction = Vector2.RIGHT
+		fish.scale.x = 1
+	else:
+		# Spawn on the right
+		spawn_x = player.position.x + spawn_offset_x
+		fish.direction = Vector2.LEFT
+		fish.scale.x = -1
+
+	# Make sure the fish doesn't spawn directly on the screen
+	var screen_left = get_viewport().get_camera_2d().global_position.x - get_viewport_rect().size.x / 2
+	var screen_right = get_viewport().get_camera_2d().global_position.x + get_viewport_rect().size.x / 2
+
+	if spawn_x > screen_left and spawn_x < screen_right:
+		if fish.direction.x > 0:
+			spawn_x = screen_left - 50  # A bit off the left side of the screen
+		else:
+			spawn_x = screen_right + 50  # A bit off the right side of the screen
+
+	fish.position = Vector2(spawn_x, y)
 	add_child(fish)
